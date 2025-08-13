b9d23f5ac6a7039469eb94ed19cba738680682b3
thiagcarvalho <thiagoc413@gmail.com>
2025-08-12 21:05:00 -0300

    feat: Create Arroba as a valueobject

---
+++ src/Core/Domain/FarmManager.Domain/ValueObjects/Arroba.cs
@@ -0,0 +1,17 @@
+﻿namespace FarmManager.Domain.ValueObject;
+
+public record Arroba
+{
+    public decimal Value { get; private set; }
+
+    public Arroba(decimal weight)
+    {
+        Value = weight;
+    }
+
+    public decimal ToKilograms() => Value * 15.0m;
+    public override string ToString()
+    {
+        return $"{Value}";
+    }
+}
