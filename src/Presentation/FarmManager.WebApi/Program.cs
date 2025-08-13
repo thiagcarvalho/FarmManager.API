1ffa3f9772119da3a2bec62f26b7fc83ac6d6db0
thiagcarvalho <thiagoc413@gmail.com>
2025-08-12 22:48:42 -0300

    feat: add service to the program.cs and creat the Animal Controller

--- src/Presentation/FarmManager.WebApi/Program.cs
+++ src/Presentation/FarmManager.WebApi/Program.cs
@@ -1,3 +1,10 @@
+using AutoMapper;
+using FarmManager.Application.Contracts.Interfaces;
+using FarmManager.Application.Contracts.Interfaces.Persistence.Queries;
+using FarmManager.Application.Services;
+using FarmManager.Persistence.Query.Store;
+using FarmManager.Persistence.Query;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +13,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IAnimalQueryRepository, AnimalQueryRepository>();
+builder.Services.AddScoped<IAnimalService, AnimalService>();
+
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+
 
 var app = builder.Build();
 
