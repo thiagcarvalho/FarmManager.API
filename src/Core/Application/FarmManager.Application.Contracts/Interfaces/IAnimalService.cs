30d40b6daed8044edacb506f368f72351d54b649
thiagcarvalho <thiagoc413@gmail.com>
2025-08-12 22:47:44 -0300

    Create a interface animal service and implement it

---
+++ src/Core/Application/FarmManager.Application.Contracts/Interfaces/IAnimalService.cs
@@ -0,0 +1,12 @@
+﻿using FarmManager.Application.Contracts.Models.InputModels;
+using FarmManager.Application.Contracts.Models.ViewModels;
+
+namespace FarmManager.Application.Contracts.Interfaces;
+
+public interface IAnimalService
+{
+    Guid SaveAnimal(AnimalInputModel animalInputModel);
+    void UpdateAnimal(Guid Id, AnimalInputModel animalInputModel);
+    AnimalViewModel? GetAnimal(Guid Id);
+    List<AnimalViewModel> GetAllAnimals();
+}
---
+++ src/Core/Application/FarmManager.Application/Services/AnimalService.cs
@@ -0,0 +1,35 @@
+﻿using FarmManager.Application.Contracts.Interfaces;
+using FarmManager.Application.Contracts.Interfaces.Persistence.Queries;
+using FarmManager.Application.Contracts.Models.InputModels;
+using FarmManager.Application.Contracts.Models.ViewModels;
+
+namespace FarmManager.Application.Services;
+
+public class AnimalService : IAnimalService
+{
+    private readonly IAnimalQueryRepository _animalQueryRepository;
+
+    public AnimalService(IAnimalQueryRepository animalQueryRepository)
+    {
+        _animalQueryRepository = animalQueryRepository;
+    }
+    public List<AnimalViewModel> GetAllAnimals()
+    {
+        return _animalQueryRepository.GetAllAnimals();
+    }
+
+    public AnimalViewModel? GetAnimal(Guid Id)
+    {
+        return _animalQueryRepository.GetAnimal(Id);
+    }
+
+    public Guid SaveAnimal(AnimalInputModel animalInputModel)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateAnimal(Guid Id, AnimalInputModel animalInputModel)
+    {
+        throw new NotImplementedException();
+    }
+}
