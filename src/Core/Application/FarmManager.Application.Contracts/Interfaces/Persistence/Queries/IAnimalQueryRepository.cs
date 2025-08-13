e2efab365d3dee4b65c08ec22a16d24c2af8a596
thiagcarvalho <thiagoc413@gmail.com>
2025-08-12 22:47:06 -0300

    feat: add interface for animal query repository

---
+++ src/Core/Application/FarmManager.Application.Contracts/Interfaces/Persistence/Queries/IAnimalQueryRepository.cs
@@ -0,0 +1,9 @@
+﻿using FarmManager.Application.Contracts.Models.ViewModels;
+
+namespace FarmManager.Application.Contracts.Interfaces.Persistence.Queries;
+
+public interface IAnimalQueryRepository
+{
+    AnimalViewModel? GetAnimal(Guid Id);
+    List<AnimalViewModel> GetAllAnimals();
+}
