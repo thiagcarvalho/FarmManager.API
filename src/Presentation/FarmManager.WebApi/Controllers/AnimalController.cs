7aba01faef69278763b24e5fb1252ed3d088c50b
thiagcarvalho <thiagoc413@gmail.com>
2025-08-12 22:48:42 -0300

    feat: add service to the program.cs and creat the Animal Controller

---
+++ src/Presentation/FarmManager.WebApi/Controllers/AnimalController.cs
@@ -0,0 +1,44 @@
+﻿using FarmManager.Application.Contracts.Interfaces;
+using FarmManager.Application.Contracts.Models.InputModels;
+using FarmManager.Domain.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace FarmManager.WebApi.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class AnimalController : ControllerBase
+    {
+        private readonly IAnimalService _animalService;
+
+        public AnimalController(IAnimalService animalService)
+        {
+            _animalService = animalService;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult Get(Guid id)
+        {
+            var animal = _animalService.GetAnimal(id);
+            return animal is null
+                ? NotFound($"Animal with ID {id} not found.")
+                : Ok(animal);
+        }
+
+        [HttpGet]
+        public ActionResult GetAll()
+        {
+            var animals = _animalService.GetAllAnimals();
+            return Ok(animals);
+        }
+
+        [HttpPost]
+        public ActionResult Post([Required] AnimalInputModel animalInputModel)
+        {
+            return Created($"/api/v1/Animal/{_animalService.SaveAnimal(animalInputModel)}", animalInputModel);
+        }
+
+    }
+}
