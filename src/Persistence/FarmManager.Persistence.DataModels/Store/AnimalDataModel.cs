944411c2b2088961973d625354d694d31e36cbc8
thiagcarvalho <thiagoc413@gmail.com>
2025-08-12 22:46:33 -0300

    feat: Create DataModels and Query Repository

---
+++ src/Persistence/FarmManager.Persistence.DataModels/Store/AnimalDataModel.cs
@@ -0,0 +1,10 @@
+﻿namespace FarmManager.Persistence.DataModels.Store;
+
+public class AnimalDataModel : DataModelBase
+{
+    public int RegisterNumber { get; set; }
+    public int Age { get; set; }
+    public decimal Weight { get; set; }
+    public string Type { get; set; } = string.Empty;
+    public DateTime Birthday { get; set; }
+}
