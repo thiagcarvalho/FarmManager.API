084633ad458b67270870c3f6bf67348cb3298806
thiagcarvalho <thiagoc413@gmail.com>
2025-08-12 22:48:10 -0300

    feat: update a filed in Animal View Model

--- src/Core/Application/FarmManager.Application.Contracts/Models/ViewModels/AnimalViewModel.cs
+++ src/Core/Application/FarmManager.Application.Contracts/Models/ViewModels/AnimalViewModel.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace FarmManager.Application.Contracts.Models.ViewModels;
 
-namespace FarmManager.Application.Contracts.Models.ViewModels
+public class AnimalViewModel
 {
-    internal class AnimalViewModel
-    {
-    }
+    public Guid Id { get; set; }
+    public int RegisterNumber { get; set; }
+    public int Age { get; set; }
+    public decimal Weight { get; set; }
+    public string Type { get; set; } = string.Empty;
+    public DateTime Birthday { get; set; }
+
 }
