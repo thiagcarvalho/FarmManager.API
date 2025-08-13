d5d11eaad0b1cc6900e99c7338c3267170643f17
thiagcarvalho <thiagoc413@gmail.com>
2025-08-12 21:05:25 -0300

    feat: create animal interface and implementation

---
+++ src/Core/Domain/FarmManager.Domain/Entities/Animal.cs
@@ -0,0 +1,27 @@
+﻿using FarmManager.Domain.Interfaces;
+using FarmManager.Domain.ValueObject;
+
+namespace FarmManager.Domain.Entities;
+
+public class Animal : IAnimal
+{
+    public int RegisterNumber { get; set; }
+    public int Age { get; set; }
+    public Arroba Weight { get; set; } = new Arroba(0);
+    public string Type { get; set; } = string.Empty;
+
+    public Animal(int registerNumber,
+        int age,
+        Arroba weight,
+        string type)
+    {
+        RegisterNumber = registerNumber;
+        Age = age;
+        Weight = weight;
+        Type = type;
+    }
+
+    public void UpdateAge(int newAge) => Age = newAge;
+
+    public void UpdateWeight(Arroba newWeight) => Weight = newWeight;
+}
---
+++ src/Core/Domain/FarmManager.Domain/Interfaces/IAnimal.cs
@@ -0,0 +1,15 @@
+﻿using FarmManager.Domain.ValueObject;
+
+namespace FarmManager.Domain.Interfaces;
+
+public interface IAnimal
+{
+    int RegisterNumber{ get; set; }
+    int Age { get; set; }
+    Arroba Weight { get; set; }
+    string Type { get; set; }
+
+    void UpdateWeight(Arroba newWeight);
+    void UpdateAge(int newAge);
+
+}
