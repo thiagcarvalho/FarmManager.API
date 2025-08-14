40c16a4bf372bae5bf056926998cd5bbfaaaa8f3
thiagcarvalho <thiagoc413@gmail.com>
2025-08-14 20:18:25 -0300

    feat: create a interface for command repository

---
+++ src/Core/Application/FarmManager.Application.Contracts/Interfaces/Persistence/Commands/IAnimalCommandRepository.cs
@@ -0,0 +1,9 @@
+﻿using FarmManager.Domain.Entities;
+
+namespace FarmManager.Application.Contracts.Interfaces.Persistence.Commands;
+
+public interface IAnimalCommandRepository
+{
+    Guid SaveAnimal(Animal animal);
+    void UpdateAnimal(Guid Id, Animal animal);
+}
