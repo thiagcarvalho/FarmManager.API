894fb2bfc9b1b00c50b105931a3441c5869c6407
thiagcarvalho <thiagoc413@gmail.com>
2025-08-14 20:18:04 -0300

    feat: add Command project to the solution

---
+++ src/Persistence/FarmManager.Persistence.Command/CommandMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using FarmManager.Domain.Entities;
+using FarmManager.Persistence.DataModels.Store;
+
+
+namespace FarmManager.Persistence.Command;
+
+public class CommandMappingProfile : Profile
+{
+    public CommandMappingProfile()
+    {
+        CreateMap<Animal, AnimalDataModel>()
+            .ForMember(dest => dest.Weight, opt => opt.MapFrom(src => src.Weight.Value));
+    }
+}
---
+++ src/Persistence/FarmManager.Persistence.Command/Store/AnimalCommandRepository.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using FarmManager.Application.Contracts.Interfaces.Persistence.Commands;
+using FarmManager.Domain.Entities;
+using FarmManager.Persistence.DataModels;
+using FarmManager.Persistence.DataModels.Store;
+
+namespace FarmManager.Persistence.Command.Store;
+
+public class AnimalCommandRepository : IAnimalCommandRepository
+{
+    private readonly IMapper _mapper;
+
+    public AnimalCommandRepository(IMapper mapper)
+    {
+        _mapper = mapper;
+    }
+
+    public Guid SaveAnimal(Animal animal)
+    {
+        var animalDataModel = _mapper.Map<AnimalDataModel>(animal);
+
+        animalDataModel.CreatedAt = DateTime.UtcNow;
+        animalDataModel.CreatedBy = "System";
+
+        MemoryStorage.Animals.Add(animalDataModel.RegisterNumber, animalDataModel);
+
+        return animalDataModel.Id;
+    }
+
+    public void UpdateAnimal(Guid Id, Animal animal)
+    {
+        throw new NotImplementedException();
+    }
+}
