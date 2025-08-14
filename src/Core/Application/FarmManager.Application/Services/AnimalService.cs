9f87dd4b17fed9b6ef7f1a0e33183a0f9658dc52
thiagcarvalho <thiagoc413@gmail.com>
2025-08-14 20:18:59 -0300

    feat: add post function to AnimalService

--- src/Core/Application/FarmManager.Application/Services/AnimalService.cs
+++ src/Core/Application/FarmManager.Application/Services/AnimalService.cs
@@ -1,17 +1,31 @@
-﻿using FarmManager.Application.Contracts.Interfaces;
+﻿using AutoMapper;
+using FarmManager.Application.Contracts.Interfaces;
+using FarmManager.Application.Contracts.Interfaces.Persistence.Commands;
 using FarmManager.Application.Contracts.Interfaces.Persistence.Queries;
 using FarmManager.Application.Contracts.Models.InputModels;
 using FarmManager.Application.Contracts.Models.ViewModels;
+using FarmManager.Domain.Entities;
+using FarmManager.Domain.Interfaces.Factories;
+using FarmManager.Domain.ValueObject;
 
 namespace FarmManager.Application.Services;
 
 public class AnimalService : IAnimalService
 {
     private readonly IAnimalQueryRepository _animalQueryRepository;
+    private readonly IAnimalCommandRepository _animalCommandRepository;
+    private readonly IAnimalFactory _animalFactory;
+    private readonly IMapper _mapper;
 
-    public AnimalService(IAnimalQueryRepository animalQueryRepository)
+    public AnimalService(IAnimalQueryRepository animalQueryRepository,
+        IAnimalCommandRepository animalCommandRepository,
+        IAnimalFactory animalFactory,
+        IMapper mapper)
     {
         _animalQueryRepository = animalQueryRepository;
+        _animalCommandRepository = animalCommandRepository;
+        _animalFactory = animalFactory;
+        _mapper = mapper;
     }
     public List<AnimalViewModel> GetAllAnimals()
     {
@@ -25,11 +39,23 @@ public class AnimalService : IAnimalService
 
     public Guid SaveAnimal(AnimalInputModel animalInputModel)
     {
-        throw new NotImplementedException();
+        var animal = CreateAnimal(animalInputModel);
+
+        return _animalCommandRepository.SaveAnimal(animal);
     }
 
     public void UpdateAnimal(Guid Id, AnimalInputModel animalInputModel)
     {
         throw new NotImplementedException();
+    }
+
+    private Animal CreateAnimal(AnimalInputModel animalInputModel)
+    {
+        return _animalFactory.Create(
+            null,
+            animalInputModel.RegisterNumber,
+            new Arroba(animalInputModel.Weight),
+            animalInputModel.Type,
+            animalInputModel.Birthday);
     }
 }
