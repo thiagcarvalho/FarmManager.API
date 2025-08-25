3810e4bebc890d896d334650b35dd96532a53aea
Thiago Carvalho <thiagoc413@gmail.com>
2025-08-25 20:57:01 -0300

    feat: create cow interface and class

---
+++ src/Core/Domain/FarmManager.Domain/Interfaces/ICow.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmManager.Domain.Interfaces
+{
+    public interface ICow : IAnimal
+    {
+        bool IsPregnant { get; set; }
+        bool HasCalf { get; set; }
+        string? Name { get; set; }
+        bool IsMilking { get; set; }
+    }
+}
