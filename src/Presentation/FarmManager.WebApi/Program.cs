39200db01622f39c40b795e34ee796f9e750129b
Thiago Carvalho <thiagoc413@gmail.com>
2025-09-20 10:44:57 -0300

    feat: update program.cs file to use config dependencies

--- src/Presentation/FarmManager.WebApi/Program.cs
+++ src/Presentation/FarmManager.WebApi/Program.cs
@@ -1,4 +1,5 @@
 using AutoMapper;
+using FarmManager.Application;
 using FarmManager.Application.Contracts.Interfaces;
 using FarmManager.Application.Contracts.Interfaces.Persistence.Commands;
 using FarmManager.Application.Contracts.Interfaces.Persistence.Queries;
@@ -19,14 +20,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<IAnimalQueryRepository, AnimalQueryRepository>();
-builder.Services.AddScoped<IAnimalCommandRepository, AnimalCommandRepository>();
-builder.Services.AddScoped<IAnimalFactory, AnimalFactory>();
-builder.Services.AddScoped<IAnimalService, AnimalService>();
 
 builder.Services.AddAutoMapper(typeof(MappingProfile),
     typeof(CommandMappingProfile));
 
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddRepositoryServices(builder.Configuration);
+builder.Services.AddFactories(builder.Configuration);
 
 var app = builder.Build();
 
