3ae3cc5f8453797904da89d27ec92da00e4a85a4
Thiago Carvalho <thiagoc413@gmail.com>
2025-09-20 10:40:42 -0300

    feat: create Config files

---
+++ src/Core/Application/FarmManager.Application/Config.cs
@@ -0,0 +1,31 @@
+﻿using FarmManager.Application.Contracts.Interfaces;
+using FarmManager.Application.Contracts.Interfaces.Persistence.Commands;
+using FarmManager.Application.Contracts.Interfaces.Persistence.Queries;
+using FarmManager.Application.Services;
+using FarmManager.Domain.AnimalFactory;
+using FarmManager.Domain.Interfaces.Factories;
+using FarmManager.Persistence.Command.Store;
+using FarmManager.Persistence.Query.Store;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FarmManager.Application;
+
+public static class Config
+{
+    public static void AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<IAnimalService, AnimalService>();
+    }
+
+    public static void AddRepositoryServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<IAnimalQueryRepository, AnimalQueryRepository>();
+        services.AddScoped<IAnimalCommandRepository, AnimalCommandRepository>();
+    }
+
+    public static void AddFactories(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<IAnimalFactory, AnimalFactory>();
+    }
+}
