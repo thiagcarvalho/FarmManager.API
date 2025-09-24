0868ecc8375055731f3e1fe8a6edede7008af90b
Thiago Carvalho <thiagoc413@gmail.com>
2025-09-23 21:59:53 -0300

    feat: add Bull Configuration

---
+++ src/Persistence/FarmManager.Persistence.EF/Configurations/BullConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using FarmManager.Persistence.DataModels.Store;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FarmManager.Persistence.EF.Configurations;
+
+public class BullConfiguration : IEntityTypeConfiguration<BullDataModel>
+{
+    public void Configure(EntityTypeBuilder<BullDataModel> builder)
+    {
+        builder.ToTable("Bulls");
+
+        builder.Property(b => b.Name)
+            .HasMaxLength(100)
+            .IsRequired(false);
+    }
+}
