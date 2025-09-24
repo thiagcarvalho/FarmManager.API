f2bb4f400804d6aad0a37be658d9179949716fa0
Thiago Carvalho <thiagoc413@gmail.com>
2025-09-23 22:00:00 -0300

    feat: add Calf Configuration

---
+++ src/Persistence/FarmManager.Persistence.EF/Configurations/CalfConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using FarmManager.Persistence.DataModels.Store;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FarmManager.Persistence.EF.Configurations;
+
+public class CalfConfiguration : IEntityTypeConfiguration<CalfDataModel>
+{
+    public void Configure(EntityTypeBuilder<CalfDataModel> builder)
+    {
+        builder.ToTable("Calves");
+
+        builder.Property(calf => calf.Gender)
+            .IsRequired();
+
+        builder.Property(calf => calf.MotherNumber)
+            .IsRequired();
+    }
+}
