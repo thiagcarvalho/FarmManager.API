c9ebb1f16fb7e245a8cd3c0dc1a46eca3c62b632
Thiago Carvalho <thiagoc413@gmail.com>
2025-10-20 22:54:17 -0300

    create loteinput and loteview models

---
+++ src/Core/Application/FarmManager.Application.Contracts/Models/InputModels/LoteInputModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FarmManager.Application.Contracts.Models.InputModels;
+
+public class LoteInputModel
+{
+    [Required(ErrorMessage = "The Lote Name is required")]
+    [MaxLength(100, ErrorMessage = "The Lote Name must be less than 100 characters")]
+    public string Name { get; set; } = string.Empty;
+}
---
+++ src/Core/Application/FarmManager.Application.Contracts/Models/ViewModels/LoteViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace FarmManager.Application.Contracts.Models.ViewModels;
+
+public class LoteViewModel
+{
+    public string Name { get; set; } = string.Empty;
+}
