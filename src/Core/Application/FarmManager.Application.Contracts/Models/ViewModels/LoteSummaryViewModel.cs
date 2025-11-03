6cba8dcaf58f65a77f139cf38301b0de43b9d0d4
Thiago Carvalho <thiagoc413@gmail.com>
2025-11-03 20:34:05 -0300

    add Lote Summary

---
+++ src/Core/Application/FarmManager.Application.Contracts/Models/ViewModels/LoteSummaryViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace FarmManager.Application.Contracts.Models.ViewModels;
+
+public class LoteSummaryViewModel
+{
+    public string LoteName { get; set; } = string.Empty;
+    public int TotalAnimals { get; set; }
+    public int Cows { get; set; }
+    public int Calves { get; set; }
+    public int Bulls { get; set; }
+}
