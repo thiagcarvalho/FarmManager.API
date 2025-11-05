8fca67a2d2a6928db0e30e9de85bf2ac6acc5d77
Thiago Carvalho <thiagoc413@gmail.com>
2025-11-05 20:24:04 -0300

    add observations datamodel

---
+++ src/Persistence/FarmManager.Persistence.DataModels/Store/ObservationDataModel.cs
@@ -0,0 +1,11 @@
+﻿namespace FarmManager.Persistence.DataModels.Store;
+
+public class ObservationDataModel
+{
+    public int Id { get; set; }
+    public Guid AnimalId { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public DateTime DataAdd { get; set; }
+
+    public AnimalDataModel Animal { get; set; } = null!;
+}
