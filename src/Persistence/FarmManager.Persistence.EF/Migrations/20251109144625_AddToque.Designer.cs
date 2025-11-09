1c51badd62e66b9683085d6ef1e3ee90d1445602
Thiago Carvalho <thiagoc413@gmail.com>
2025-11-09 11:56:37 -0300

    add datamodel for toque and mgiration

--- src/Persistence/FarmManager.Persistence.EF/Migrations/20251109144625_AddToque.Designer.cs
+++ src/Persistence/FarmManager.Persistence.EF/Migrations/20251109144625_AddToque.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FarmManager.Persistence.EF.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace FarmManager.Persistence.EF.Migrations
 {
     [DbContext(typeof(FarmManagerDbContext))]
-    partial class FarmManagerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251109144625_AddToque")]
+    partial class AddToque
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Persistence/FarmManager.Persistence.EF/Migrations/20251109144625_AddToque.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace FarmManager.Persistence.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddToque : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Toques",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    cowId = table.Column<int>(type: "integer", nullable: false),
+                    dataToque = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    vacaPrenha = table.Column<bool>(type: "boolean", nullable: false),
+                    tempoGestacaoDias = table.Column<int>(type: "integer", nullable: false),
+                    observacoes = table.Column<string>(type: "character varying(2000)", maxLength: 2000, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Toques", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Toques_Cows_cowId",
+                        column: x => x.cowId,
+                        principalTable: "Cows",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Toques_cowId",
+                table: "Toques",
+                column: "cowId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Toques");
+        }
+    }
+}
