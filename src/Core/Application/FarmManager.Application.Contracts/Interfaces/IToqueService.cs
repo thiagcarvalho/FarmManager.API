870c327c57048c7466b7dc1d9a190f95152eefa7
Thiago Carvalho <thiagoc413@gmail.com>
2025-11-09 12:24:38 -0300

    add toque command and query repository and add toque service

---
+++ src/Core/Application/FarmManager.Application.Contracts/Interfaces/IToqueService.cs
@@ -0,0 +1,13 @@
+﻿using FarmManager.Application.Contracts.Models.InputModels;
+using FarmManager.Application.Contracts.Models.ViewModels;
+
+namespace FarmManager.Application.Contracts.Interfaces;
+
+public interface IToqueService
+{
+    void AddToque(ToqueInputModelcs toqueInputModelcs);
+    void DeleteToque(int id);
+    ToqueViewModel? GetToque(int id);
+    List<ToqueViewModel> GetByAnimalId(Guid animalId);
+    List<ToqueViewModel> GetAll();
+}
---
+++ src/Core/Application/FarmManager.Application.Contracts/Interfaces/Persistence/Commands/IToqueCommandRepository.cs
@@ -0,0 +1,9 @@
+﻿using FarmManager.Application.Contracts.Models.InputModels;
+
+namespace FarmManager.Application.Contracts.Interfaces.Persistence.Commands;
+
+public interface IToqueCommandRepository
+{
+    void AddToque(ToqueInputModelcs toqueInputModelcs);
+    void DeleteToque(int id);
+}
