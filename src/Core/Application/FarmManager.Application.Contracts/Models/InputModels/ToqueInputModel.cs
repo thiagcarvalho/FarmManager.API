d67e3c7b6d6a74b286fcee4540d00bec61a25461
Thiago Carvalho <thiagoc413@gmail.com>
2025-11-09 12:17:54 -0300

    add toque view and input model

---
+++ src/Core/Application/FarmManager.Application.Contracts/Models/InputModels/ToqueInputModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FarmManager.Application.Contracts.Models.InputModels;
+
+public class ToqueInputModelcs
+{
+
+    [MaxLength(ErrorMessage = "Data Prenha is required")]
+    public DateTime DataToque { get; set; }
+
+    [MaxLength(ErrorMessage = "Vaca Prenha is required")]
+    public bool VacaPrenha { get; set; }
+
+    [MaxLength(ErrorMessage = "Tempo de Gestacao is required")]
+    public int TempoGestacaoDias { get; set; }
+
+    [MaxLength(100, ErrorMessage = "The observation cannot exceed 100 characters")]
+    public string Observacoes { get; set; } = string.Empty;
+}
---
+++ src/Core/Application/FarmManager.Application.Contracts/Models/ViewModels/ToqueViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmManager.Application.Contracts.Models.ViewModels;
+
+public class ToqueViewModel
+{
+    public int Id { get; set; }
+    public DateTime DataToque { get; set; }
+    public bool VacaPrenha { get; set; }
+    public int TempoGestacaoDias { get; set; }
+    public string Observacoes { get; set; } = string.Empty;
+    public DateTime DataPartoPrevisto { get; set; }
+}
